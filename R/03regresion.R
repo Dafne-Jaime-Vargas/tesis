30ccc0a15e69216a214e77be7dd6629c186c7f78
Dafne Jaime Vargas <67875879+Dafne-Jaime-Vargas@users.noreply.github.com>
2022-07-15 18:48:27 -0400

    Update 03regresion.R

--- R/03regresion.R
+++ R/03regresion.R
@@ -96,7 +96,7 @@ writexl::write_xlsx(ajuste_modelo_reg, "output/tablas/03_ajuste_regresion.xlsx")
 
 # visualización del modelo ------------------------------------------------
 
-tab_model(multinom8,
+tab_model(multinom3,
           string.intercept = "(Intercepto)",
           dv.labels = c("Modelo"),
           string.pred = "Predictores",
@@ -110,97 +110,22 @@ tab_model(multinom8,
           show.reflvl = F,
           digits = 2,
           encoding = "latin9",
-          file = "output/regresion6.doc",
+          file = "output/regresion1.doc",
           collapse.ci = F) #deja los ci en una fila aparte
 
 
-# procesar los datos ------------------------------------------------------
+# graficar ----------------------------------------------------------------
 
-datos <- descriptivos %>% mutate(clases2 = case_when(`M6_or$predclass`%in% c(1,3,4) ~ "clase 1, 3 y 4 PE",
-                                                     `M6_or$predclass`%in% c(2,6) ~ "clase 2 y 6 PM",
-                                                     `M6_or$predclass`==5 ~ "clase 5 PMA",
-                                                     TRUE ~ NA_character_))
-
-#estimar la clase de referencia
-#datos$clases <- relevel(datos$clases, ref = 'clase de precariedad escaza (1 y 4)')
-
-
-# estimación del modelo logístico -----------------------------------------
-#modelo nulo
-
-#estimar la clase de referencia
-datos$educacion <- relevel(datos$educacion, ref = 'Postgrado')
-
-
-multinom_1<- multinom(clases2 ~ sexo, data = datos, weights = factor_XS)
-
-summary(multinom_1)
-
-multinom_2 <- multinom(clases2 ~ sexo + edad_tramos, data = datos, weights = factor_XS)
-
-summary(multinom_2)
-
-multinom_3 <- multinom(clases2 ~ sexo + edad_tramos + educacion, data = datos, weights = factor_XS)
-
-summary(multinom_3)
-
-# ajuste de modelos -------------------------------------------------------
-# ----- tabla de ajuste ----
-aic1 <- multinom_1$AIC
-aic2 <- multinom_2$AIC
-aic3 <- multinom_3$AIC
-
-log1 <- multinom_1$value
-log2 <- multinom_2$value
-log3 <- multinom_3$value
-
-
-p1 <- r2_mcfadden(multinom_1)[1]
-p2 <- r2_mcfadden(multinom_2)[1]
-p3 <- r2_mcfadden(multinom_3)[1]
-
-
-v1 <- r2_mcfadden(multinom_1)[2]
-v2 <- r2_mcfadden(multinom_2)[2]
-v3 <- r2_mcfadden(multinom_3)[2]
-
-
-ajuste_modelo_reg2 <-data.frame(c("Modelo 1","Modelo 2", "Modelo 3"),
-                               c(aic1, aic2, aic3),
-                               c(log1, log2, log3),
-                               c(p1[["R2"]], p2[["R2"]], p3[["R2"]]),
-                               c(v1[["R2_adjusted"]], v2[["R2_adjusted"]], v3[["R2_adjusted"]]))
-
-
-colnames(ajuste_modelo_reg2)<-c("Modelo", "AIC", "Loglikelihood", "R2 Mcfadden", "R2 Mcfadden ajustado" )
-
-writexl::write_xlsx(ajuste_modelo_reg2, "output/tablas/03_ajuste_regresion.xlsx")
-
-# visualización del modelo ------------------------------------------------
-
-tab_model(multinom_3,
-          string.intercept = "(Intercepto)",
-          dv.labels = c("Modelo"),
-          string.pred = "Predictores",
-          p.style = "stars",
-          transform = NULL,
-          show.ci = .95,
-          show.intercept = T,
-          string.ci = "IC 95%",
-          show.aic = T, #mostrar aic
-          show.loglik = T, #mostrar likelihood
-          show.reflvl = F,
-          digits = 2,
-          encoding = "latin9",
-          #file = "output/regresion3.doc",
-          collapse.ci = F) #deja los ci en una fila aparte
-
-sjPlot::plot_model(multinom_3, 
+sjPlot::plot_model(multinom8, 
                    show.p = T,
                    show.values =  T,
                    ci.lvl = 0.95, 
+                   show.intercept = T,
                    title = "Estimación de predictores",
                    vline.color = "green")
 
+# guardar -----------------------------------------------------------------
+
+save(list = ls(), file = "output/regresion.RData")
 
 
